756a2fd0eea154f651704cd5a6b6ff27c0c880a1
MacroLions <00175617@uca.edu.sv>
2018-07-12 16:34:42 -0600

    Ventanas completas!

--- src/Ventanas/Primera.java
+++ src/Ventanas/Primera.java
@@ -7,6 +7,8 @@ package Ventanas;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -14,14 +16,31 @@ import javax.swing.JPanel;
  * @author Mai Perez
  */
 public class Primera extends JPanel {
-    public int WIDTH = 500;
-    public int HEIGHT = 500;
+    public int WIDTH = 200;
+    public int HEIGHT = 100;
+    static JButton White = new JButton("Black!");
     
     public Primera(){
+        White.setBounds(10, 30, 190, 50);
+        White.setBackground(Color.white);
+        White.setForeground(Color.black);
+        White.setFont(new Font("arial",Font.BOLD,50));
+        
+        add(White);
         setBackground(Color.black);
         setLayout(null);
         setPreferredSize(new Dimension(WIDTH,HEIGHT));
         validate();
     
     }
+
+    public static JButton getWhite() {
+        return White;
+    }
+
+    public static void setWhite(JButton White) {
+        Primera.White = White;
+    }
+    
+    
 }
--- src/Ventanas/Segunda.java
+++ src/Ventanas/Segunda.java
@@ -7,6 +7,8 @@ package Ventanas;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -14,14 +16,32 @@ import javax.swing.JPanel;
  * @author Mai Perez
  */
 public class Segunda extends JPanel {
-    public int WIDTH = 500;
-    public int HEIGHT = 500;
+    public int WIDTH = 200;
+    public int HEIGHT = 100;
+    static JButton Black = new JButton("White");
     
     public Segunda(){
-        setBackground(Color.black);
+        Black.setBounds(10, 30, 190, 50);
+        Black.setBackground(Color.black);
+        Black.setForeground(Color.white);
+        Black.setFont(new Font("arial",Font.BOLD,50));
+        
+        add(Black);
+        Black.setVisible(false);
+        setBackground(Color.white);
         setLayout(null);
         setPreferredSize(new Dimension(WIDTH,HEIGHT));
         validate();
     
     }
+
+    public static JButton getBlack() {
+        return Black;
+    }
+
+    public static void setBlack(JButton Black) {
+        Segunda.Black = Black;
+    }
+    
+    
 }
